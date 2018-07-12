f6844eeb05fbb875c97af6a7d8988cca952c6cf5
Mona Fenzl <mona@MacBook-Pro-106.local>
2018-07-12 18:29:26 +0200

    refactored Diamond class

--- src/exercises/Diamond.java
+++ src/exercises/Diamond.java
@@ -2,24 +2,29 @@ package exercises;
 
 public class Diamond {
 
-    private void printWithPadding(int chars, int stars) {
-        int spaces = chars - stars;
+    private void printWithPadding(int positions, int chars) {
+
+        int spaces = positions - chars;
         for (int s = spaces / 2; s > 0; s--) {
             System.out.print(" ");
         }
+    }
 
+    private void printStars(int positions, int stars) {
+
+        printWithPadding(positions, stars);
         for (int s = stars; s > 0; s--) {
             System.out.print("*");
         }
     }
 
     private void upperHalf(int lines) {
-        int chars = lines*2 - 1;
+        int positions = lines*2 - 1;
         int stars = 1;
 
-        for (int line = 1; line < chars; line++) {
+        for (int line = 1; line < positions; line++) {
             if (line % 2 > 0) {
-                printWithPadding(chars, stars);
+                printStars(positions, stars);
                 stars += 2;
 
             } else {
@@ -29,13 +34,13 @@ public class Diamond {
     }
 
     private void lowerHalf(int lines) {
-        int chars = lines*2 - 1;
-        int stars = chars;
+        int positions = lines*2 - 1;
+        int stars = positions;
 
-        for (int line = chars; line >= 0; line--) {
+        for (int line = positions; line >= 0; line--) {
             if (line % 2 > 0) {
                 stars -= 2;
-                printWithPadding(chars, stars);
+                printStars(positions, stars);
 
             } else {
                 System.out.println("");
@@ -44,9 +49,9 @@ public class Diamond {
     }
 
     private void middleRow(int lines) {
-        int chars = lines*2 - 1;
-        int maxStars = chars;
-        printWithPadding(chars, maxStars);
+        int positions = lines*2 - 1;
+        int maxStars = positions;
+        printStars(positions, maxStars);
         System.out.println("");
     }
 
@@ -66,16 +71,9 @@ public class Diamond {
 
     public void diamondName(int lines, String name) {
         this.upperHalf(lines);
-        System.out.println("");
-
-        int chars = lines*2 - 1;
-        int spaces = chars - name.length();
-        for (int s = spaces / 2; s > 0; s--) {
-            System.out.print(" ");
-        }
+        int positions = lines*2 - 1;
+        printWithPadding(positions, name.length());
         System.out.println(name);
-        System.out.println("");
-
         this.lowerHalf(lines);
 
     }
