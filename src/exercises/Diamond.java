3b0797af6cea256552f4300bf13e231313d26161
Mona <>
2018-07-13 19:35:45 +0200

    refactoring of Diamond class

--- src/exercises/Diamond.java
+++ src/exercises/Diamond.java
@@ -2,79 +2,67 @@ package exercises;
 
 public class Diamond {
 
-    private void printWithPadding(int positions, int chars) {
-
-        int spaces = positions - chars;
-        for (int s = spaces / 2; s > 0; s--) {
+    private void printPadding(int lines, int chars) {
+        int max = lines * 2 - 1;
+        int spaces = (max - chars)/2;
+        for (int s = spaces; s > 0; s--) {
             System.out.print(" ");
         }
     }
 
-    private void printStars(int positions, int stars) {
-
-        printWithPadding(positions, stars);
+    private void printStars(int stars) {
         for (int s = stars; s > 0; s--) {
             System.out.print("*");
         }
     }
 
-    private void upperHalf(int lines) {
-        int positions = lines*2 - 1;
-        int stars = 1;
-
-        for (int line = 1; line < positions; line++) {
+    private void printTriangle(int stars, int lines, int s) {
+        int max = lines * 2 - 1;
+        for (int line = 1; line < max; line++) {
             if (line % 2 > 0) {
-                printStars(positions, stars);
-                stars += 2;
-
+                printPadding(lines, stars);
+                printStars(stars);
+                stars += s;
             } else {
                 System.out.println("");
             }
         }
+    }
+
+    private void upperHalf(int lines) {
+        int stars = 1;
+        int starsAdded = 2;
+        printTriangle(stars, lines, starsAdded);
     }
 
     private void lowerHalf(int lines) {
-        int positions = lines*2 - 1;
-        int stars = positions;
-
-        for (int line = positions; line >= 0; line--) {
-            if (line % 2 > 0) {
-                stars -= 2;
-                printStars(positions, stars);
-
-            } else {
-                System.out.println("");
-            }
-        }
+        int stars = lines * 2 - 3;
+        int starsSubtracted = -2;
+        printTriangle(stars, lines, starsSubtracted);
     }
 
     private void middleRow(int lines) {
-        int positions = lines*2 - 1;
-        int maxStars = positions;
-        printStars(positions, maxStars);
+        int stars = lines * 2 - 1;;
+        printPadding(lines, stars);
+        printStars(stars);
         System.out.println("");
     }
-
 
     public void isoscelesTriangle(int lines) {
         this.upperHalf(lines);
         this.middleRow(lines);
-
     }
 
     public void diamond(int lines) {
         this.upperHalf(lines);
         this.middleRow(lines);
         this.lowerHalf(lines);
-
     }
 
     public void diamondName(int lines, String name) {
         this.upperHalf(lines);
-        int positions = lines*2 - 1;
-        printWithPadding(positions, name.length());
+        printPadding(lines, name.length());
         System.out.println(name);
         this.lowerHalf(lines);
-
     }
 }
